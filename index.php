e7562997c1766277c85be36365d4d81a9bb08a1f
bkaczmarczyk <45500658+bkaczmarczyk@users.noreply.github.com>
2019-04-20 17:10:08 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$description = $_POST['description'];
+  $status = $_POST['status'];
+  $currency = $_POST['currency'];
+  $amount = $_POST['amount'];
+  $transaction_type = "S";
+  $hash = $_POST['hash'];
+  ?>
+ <html>
+<form action="https://moosh.pl/cart/payment?c=1" method="post">
+    <input type="hidden" name="status" value="<?php echo $status ?>">
+    <input type="hidden" name="description" value="<?php echo $description ?>">
+    <input type="hidden" name="currency" value="<?php echo $currency ?>">
+    <input type="hidden" name="amount" value="<?php echo $amount ?>" />
+    <input type="hidden" name="hash" value="<?php echo $hash ?>" />
+    <input type="hidden" name="transaction_type" value="<?php echo $transaction_type ?>" />
+    <button type="submit">Zapłać z PeP</button>
+    
+<script type="text/javascript">
+window.onload = function() {
+   document.forms["EventConfirmRedirection"].submit();
+}
+</script>
+</html>
